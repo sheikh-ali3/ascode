be6a3e4c35d105714db8b60169d26c406e01a78d
Muhammad Ali <sheikhali12332@gmail.com>
2024-09-27 11:05:01 +0500

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,32 @@
-import Image from "next/image";
+import React from 'react'
+import Image from 'next/image'
 
-export default function Home() {
+function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div className='bg-black fomt-Arial'>
+      <nav className='m-[10px] bg-black text-white p-[30px] text-center text-[30px] relative'>
+        <Image className='mr-[10px] float-left mt-[10px] pl-[30px] pr-[20px]' src="/public/Systems and technology programming company logo.png" alt="Logo" width={100} height={50}/>
+        <h2 className='inline mr-[10px] bg-black float-left text-center pr-[500px]'><a href="home.html">Ascode Solutions</a></h2>
+        <ul>
+            <li className='inline mr-[10px] pl-[20px]'><a href="home.html" className="text-[#33FF00]">Home</a></li>
+            <li className='inline mr-[10px] pl-[20px]'><a href="#about">About</a></li>
+            <li className='inline mr-[10px] pl-[20px]'>
+                <a href="services.html">Services<span></span></a>
+                <ul className='none absolute bg-black text-left pr-[1500px]'>
+                    <li className='block p-[10px]'><a href="web-dev.html">Web Development</a></li>
+                    <li className='block p-[10px]'><a href="mobile-app-dev.html">Mobile Development</a></li>
+                    <li className='block p-[10px]'><a href="desktop-dev.html">Desktop Development</a></li>
+                    <li className='block p-[10px]'><a href="database.html">Database Development</a></li>
+                    <li className='block p-[10px]'><a href="UI-UX.html">UI/UX</a></li>
+                    <li className='block p-[10px]'><a href="seo.html">SEO</a></li>
+                    <li className='block p-[10px]'><a href="digital-markt.html">Digital Marketing</a></li>
+                </ul>
+            </li>
+            <li className='inline mr-[10px] pl-[20px]'><a href="#footer">Contact</a></li>
+        </ul>
+      </nav>
     </div>
-  );
+  )
 }
+
+export default Home
