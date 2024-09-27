b9c54cc92ef5b49be428efa3ed2bb7b7b78f10f8
Muhammad Ali <sheikhali12332@gmail.com>
2024-09-27 11:05:13 +0500

    1.1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,14 @@ function Home() {
   return (
     <div className='bg-black fomt-Arial'>
       <nav className='m-[10px] bg-black text-white p-[30px] text-center text-[30px] relative'>
-        <Image className='mr-[10px] float-left mt-[10px] pl-[30px] pr-[20px]' src="/public/Systems and technology programming company logo.png" alt="Logo" width={100} height={50}/>
+        <Image className='mr-[10px] float-left pl-[30px] pr-[20px]' src="/Systems and technology programming company logo.png" alt="Logo" width={100} height={50}/>
         <h2 className='inline mr-[10px] bg-black float-left text-center pr-[500px]'><a href="home.html">Ascode Solutions</a></h2>
         <ul>
             <li className='inline mr-[10px] pl-[20px]'><a href="home.html" className="text-[#33FF00]">Home</a></li>
             <li className='inline mr-[10px] pl-[20px]'><a href="#about">About</a></li>
-            <li className='inline mr-[10px] pl-[20px]'>
-                <a href="services.html">Services<span></span></a>
-                <ul className='none absolute bg-black text-left pr-[1500px]'>
+            <li className='inline mr-[10px] pl-[20px] relative'>
+                <a className=' inline-block bg-red-400' href="services.html">Services<span></span></a>
+                <ul className='absolute left-0  bg-yellow-400 w-[200px] text-left '>
                     <li className='block p-[10px]'><a href="web-dev.html">Web Development</a></li>
                     <li className='block p-[10px]'><a href="mobile-app-dev.html">Mobile Development</a></li>
                     <li className='block p-[10px]'><a href="desktop-dev.html">Desktop Development</a></li>
